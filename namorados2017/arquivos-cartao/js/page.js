f6de2df18e99c044750de22db2c2181cdd332297
santanche <santanche@tapejara>
2018-06-17 13:53:27 -0300

    Mudança no esquema de pastas para aparecer um único HTML na raiz.

--- namorados2017/arquivos-cartao/js/page.js
+++ namorados2017/arquivos-cartao/js/page.js
@@ -3,13 +3,15 @@ function updateHeader() {
 	
 	parentWindow.getElementById("navigator_text").innerHTML =
         document.getElementById("description").getAttribute("content");
+
+	var mainContext = parentWindow.getElementById("main_context").getAttribute("content");
         
     var navigatorPrevious =
         document.getElementById("navigator_previous");
     var navigatorBackward = parentWindow.getElementById("navigator_backward");
     if (navigatorPrevious != null) {
     	navigatorBackward.className = "navigator_backward_enabled";
-        navigatorBackward.innerHTML = "<a href='" + navigatorPrevious.getAttribute("content") +
+        navigatorBackward.innerHTML = "<a href='" + mainContext + navigatorPrevious.getAttribute("content") +
             "' target='render'><div class='navigator_backward_enabled'></div></a>";
     } else {
     	navigatorBackward.className = "navigator_backward_disabled";
@@ -21,7 +23,7 @@ function updateHeader() {
     var navigatorForward = parentWindow.getElementById("navigator_forward");
     if (navigatorNext != null) {
     	navigatorForward.className = "navigator_forward_enabled";
-        navigatorForward.innerHTML = "<a href='" + navigatorNext.getAttribute("content") +
+        navigatorForward.innerHTML = "<a href='" + mainContext + navigatorNext.getAttribute("content") +
             "' target='render'><div class='navigator_forward_enabled'></div></a>";
     } else {
     	navigatorForward.className = "navigator_forward_disabled";
