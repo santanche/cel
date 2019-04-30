2a52df924bd30f10dd2b5403a1628533e398fb1f
André Santanchè <santanche@gmail.com>
2019-04-30 15:19:12 -0300

    Tour Guiado

--- aniversario2019/aniversario2019.js
+++ aniversario2019/aniversario2019.js
@@ -4,6 +4,7 @@ start() {
   this.imageZoom = this.imageZoom.bind(this);
   this.zoomInClicked = this.zoomInClicked.bind(this);
   this.zoomOutClicked = this.zoomOutClicked.bind(this);
+  this.nextClicked = this.nextClicked.bind(this);
 
   this._treeImage = document.querySelector("#tree-image");
   this._treeImage.addEventListener("click", this.imageZoom, false);
@@ -14,55 +15,92 @@ start() {
   this._buttonZoomOut = document.querySelector("#button-out");
   this._buttonZoomOut.addEventListener("click", this.zoomOutClicked, false);
 
+  this._buttonNext = document.querySelector("#button-next");
+  this._buttonNext.addEventListener("click", this.nextClicked, false);
+
   this._treeImage.addEventListener("click", this.imageZoomIn, false);
-  // this._state = 0;
+
   this._dimensions = this.screenDimensions();
   this._shiftX = 0;
   this._shiftY = 0;
   this._scale = 1;
   this._directionIn = true;
+  this._storyCounter = 1;
+
+  this._pathPos = 0;
+  this._lastScale = 1;
+  this._lastTransX = 0;
+  this._lastTransY = 0;
+
+  this._duration = 0;
 }
 
 zoomInClicked() {
   this._buttonZoomIn.style.display = "none";
-  this._buttonZoomOut.style.display = "block";
+  this._buttonZoomOut.style.display = "inline";
   this._directionIn = false;
 }
 
 zoomOutClicked() {
   this._buttonZoomOut.style.display = "none";
-  this._buttonZoomIn.style.display = "block";
+  this._buttonZoomIn.style.display = "inline";
   this._directionIn = true;
 }
 
-imageZoom(event) {
-  this._shiftX = this._shiftX +
-    ((this._dimensions.width / 2) - event.clientX) / this._scale;
-  this._shiftY = this._shiftY +
-    ((this._dimensions.height / 2) - event.clientY) / this._scale;
+nextClicked() {
+  const next = Zoom.path[this._pathPos];
+  this.zoomTo(next[0], next[1], next[2]);
+  this._pathPos++;
+}
 
-  this._scale = (this._directionIn) ? this._scale * 2 : this._scale / 2;
+zoomTo(scale, transX, transY) {
+  this._treeImage.style.transform =
+    "scale(" + scale +
+    ") translate(" + transX + "px," + transY + "px)";
+
+  this._lastScale = scale;
+  this._lastTransX = transX;
+  this._lastTransY = transY;
+}
+
+zoomToAnim(scale, transX, transY) {
+  let rule = this.findKeyframesRule("tree-anim");
+
+  rule.deleteRule("0%");
+  rule.deleteRule("100%");
+
+  rule.appendRule("0% {transform: scale(" + this._lastScale +
+                    ") translate(" + this._lastTransX + "px," +
+                    this._lastTransY + "px)}");
+  rule.appendRule("100% {transform: scale(" + scale +
+                    ") translate(" + transX + "px," +
+                    transY + "px)}");
+
+  this._duration += 5;
+  this._treeImage.style.animationDuration = this._duration + "s";
+  this._treeImage.style.animationFillMode = "forwards";
+  this._treeImage.style.animationName = "tree-anim";
+
+  this._lastScale = scale;
+  this._lastTransX = transX;
+  this._lastTransY = transY;
+}
+
+imageZoom(event) {
+  console.log("x: " + event.clientX + "; y: " + event.clientY);
+  const transX = this._lastTransX +
+    ((this._dimensions.width / 2) - event.clientX) / this._lastScale;
+  const transY = this._lastTransY +
+    ((this._dimensions.height / 2) - event.clientY) / this._lastScale;
+
+  const scale = (this._directionIn) ? this._lastScale * 2 : this._lastScale / 2;
+  console.log("scale(" + scale +
+    ") translate(" + transX + "px," + transY + "px)");
+  this.zoomTo(scale, transX, transY);
+  /*
   this._treeImage.style.transform =
     "scale(" + this._scale +
     ") translate(" + this._shiftX + "px," + this._shiftY + "px)";
-
-  /*
-  if (this._state <= 4) {
-    this._shiftX = this._shiftX +
-      ((this._dimensions.width / 2) - event.clientX) / this._scale;
-    this._shiftY = this._shiftY +
-      ((this._dimensions.height / 2) - event.clientY) / this._scale;
-    this._scale *= 2;
-    this._treeImage.style.transform =
-      "scale(" + this._scale +
-      ") translate(" + this._shiftX + "px," + this._shiftY + "px)";
-  } else {
-    this._treeImage.style.transform = "";
-    this._shiftX = 0;
-    this._shiftY = 0;
-    this._scale = 1;
-  }
-  this._state = (this._state + 1) % 6;
   */
 }
 
@@ -85,8 +123,51 @@ screenDimensions() {
   return dimensions;
 }
 
+findKeyframesRule(rule) {
+  console.log(rule);
+  let ss = document.styleSheets;
+  for (let i = 0; i < ss.length; i++) {
+    console.log(ss[i]);
+    for (let j = 0; j < ss[i].cssRules.length; j++) {
+      if (ss[i].cssRules[j].type == window.CSSRule.KEYFRAMES_RULE && 
+      ss[i].cssRules[j].name == rule) { 
+        return ss[i].cssRules[j]; }
+    }
+  }
+  return null;
+}
+
 }
 
 (function() {
+
+   Zoom.path = [[4, 515, 76],
+                [8, 564, 74],
+                [8, 578, 24],
+                [8, 503, 24],
+                [1, 0, 0],
+                [4, 360, -106],
+                [8, 360, -106],
+                [32, 360, -106],
+                [128, 360, -104],
+                [128, 360, -107],
+                [1, 0, 0],
+                [10, 133, -91],
+                [20, 133, -91],
+                [50, 133, -87],
+                [50, 133, -95],
+                [1, 0, 0],
+                [4, -130, -29],
+                [16, -53, -28],
+                [16, -201, -40],
+                [1, 0, 0],
+                [4, -402, 68],
+                [16, -402, 68],
+                [2, -589, 61],
+                [8, -562, -79],
+                [32, -560, -113],
+                [128, -554, -122],
+                [128, -566, -121]];
+
    Zoom.instance = new Zoom();
 })();
